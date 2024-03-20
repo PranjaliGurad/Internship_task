25b57f1b95d1626961b502418fd924d3129f8686
PranjuG <pranjaligurad@gmail.com>
2024-03-20 16:03:01 +0530

    Initial Commit

---
+++ cleaning.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import nltk
+from nltk.stem import PorterStemmer
+import re
+from nltk.corpus import stopwords
+
+
+df = pd.read_csv("DataNeuron_Text_Similarity.csv")
+
+all_stopwords = stopwords.words("english")
+all_stopwords.remove("not")
+all_stopwords.remove("nor")
+all_stopwords.remove("no")
+
+ps = PorterStemmer()
+
+corpus1 = []
+for i in range(df["text1"].shape[0]):
+    rev = re.sub("[^a-zA-Z]"," " ,df["text1"][i])
+    rev = rev.lower()
+    rev = rev.split()
+    rev = [ps.stem(word) for word in rev if not word in set(all_stopwords) ]
+    rev = " ".join(rev)
+    corpus1.append(rev)
+
+corpus2 = []
+for i in range(df["text2"].shape[0]):
+    rev = re.sub("[^a-zA-Z]"," " ,df["text2"][i])
+    rev = rev.lower()
+    rev = rev.split()
+    rev = [ps.stem(word) for word in rev if not word in set(all_stopwords) ]
+    rev = " ".join(rev)
+    corpus2.append(rev)
+    
+data = pd.DataFrame([corpus1,corpus2]).T
+data = data.rename({0:"text1", 1:"text2"}, axis= 1)
+
---
+++ solution.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import spacy
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+from cleaning import data
+
+df = data
+
+# Load the pre-trained spaCy model
+nlp = spacy.load("en_core_web_md")
+
+def calculate_similarity(text1, text2):
+    # Process the text with spaCy
+    doc1 = nlp(text1)
+    doc2 = nlp(text2)
+    
+    # Calculate the average word embeddings for each document
+    avg_vector1 = doc1.vector
+    avg_vector2 = doc2.vector
+    
+    # Compute the cosine similarity between the vectors
+    similarity_score = cosine_similarity([avg_vector1], [avg_vector2])[0][0]
+    
+    return similarity_score
+
+# Streamlit UI
+st.title('Text Similarity Scorer')
+
+text1 = st.text_area('Enter Text 1', '')
+text2 = st.text_area('Enter Text 2', '')
+
+if st.button('Calculate Similarity'):
+    if text1 and text2:
+        similarity_score = calculate_similarity(text1, text2)
+        st.write('Similarity Score:', similarity_score)
+    else:
+        st.write('Please enter both text inputs.')
